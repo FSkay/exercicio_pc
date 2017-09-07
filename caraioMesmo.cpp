2f1187cc18e62bc1129132264fb34247239b47eb
Felipe Menegat <felipe_menegat@hotmail.com>
2017-09-07 00:42:42 -0300

    Add files via upload

---
+++ caraioMesmo.cpp
@@ -0,0 +1,67 @@
+/*
+|---------------------------------------------------------------------------------------------|	
+|	 Fazer um programa em C que apresente a idade de uma pessoa em Anos, Meses e Dias.
+|	Por exemplo, o usuário informa a data de nascimento e a data atual e o sistema deve apresentar uma saída do tipo:
+|	28 anos, 08 meses e 12 dias
+|	Regras:
+|	1) Não é permitido utilizar as funções de data do C (as outras funções podem ser utilizadas)
+|	2) Considerar anos Bissextos
+|	3) O programa deve compilar
+|	4) Cada aluno(a) deverá apresentar e explicar o programa para o Professor                 |
+|	5) Os resultados podem ser conferidos em (respeitando a notação):
+|	http://www.convertunits.com/dates/
+|	http://www.timeanddate.com/date/duration.html                                             |
+|	6) Código bem indentado e bons nomes de funções e variáveis valem pontuação extra.        |
+|	                                                                                          |
+|	                                                                                          |
+|	Aluno F.Menegat                                                                           |
+|---------------------------------------------------------------------------------------------|
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+//-----FUNÇÕES-----
+	
+ int calculoIdade(int N1, N2){
+ 	
+ 	
+ 	
+ 	
+ }
+	
+
+//-----CHAMADAS DAS FUNÇÕES------
+
+//Prototipo de função bissesto
+float bissexto (int ano);
+
+//Prototipo de função calculo de idade
+int calculoIdade (int N1, int N2);
+
+
+// Estrutura ano
+struct ano {
+	int ano; 
+	int mes;
+	int dia;
+	int idade;
+};
+ano 
+
+//Variaveis
+int anoNasc
+
+//Inicio do programa
+int main (){
+	setlocale(LC_ALL, "");	
+	ano Ano;
+	
+	
+	printf("\nDigite dia, mês e ano de nascimento: \n");
+	scanf("%d %d %d", Ano.ano, Ano.dia, Ano.mes);
+	
+	
+
+}

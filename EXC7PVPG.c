885c3787afd191011f6a1d4c4b72ceaa0e8e50c5
Felipe Menegat <felipe_menegat@hotmail.com>
2017-09-14 19:16:56 -0300

    Create EXC7PVPG.c

---
+++ EXC7PVPG.c
@@ -0,0 +1,34 @@
+/*7.Faça um programa que determina o máximo e o mínimo de um conjunto de n números inteiros armazenados 
+ num vetor A de 10 elementos. 
+
+Aluno: Felipe Menegat
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+
+int f[10];
+int d ;
+int maximo ;
+maximo = 0 ;
+int minimo ;
+	
+	for(d=0;d<10;d++){
+		printf("Digite o valor de d[%d]:",d+1);
+		fflush(stdin);
+		scanf("%d",&f);
+		if(f>maximo){
+		maximo=f;
+		}
+		if(f<v(d-1)){
+		minimo=v;
+		}
+	}
+printf("Maximo e:%d\n",maximo);
+printf("Minimo e:%d\n",minimo);
+system("PAUSE");
+return 0;
+}

09bc9ffad56a3742995fe39a0de0e9ff231bc868
Felipe Menegat <felipe_menegat@hotmail.com>
2017-09-14 07:23:55 -0300

    Create EX4PGPV.c

---
+++ EX4PGPV.c
@@ -0,0 +1,36 @@
+/*4.	Durante uma corrida de automóveis com N voltas de duração foram anotados para um piloto, na ordem, os tempos registrados em cada volta. 
+Fazer um programa em C para ler os tempos das N voltas, calcular e imprimir: 
+o	melhor tempo; 
+o	a volta em que o melhor tempo ocorreu; 
+o	tempo médio das N voltas; 
+
+Aluno: Felipe Menegat
+*/
+
+#include <stdio.h> 
+#include <stdlib.h> 
+
+int main() { 
+
+float tmedio; 
+int tempo[10],mt=-1,st=0; 
+int melhorvolta,i; 
+
+for(i=0;i<10;i++) {
+ 
+printf("\ndigite o tempo obtido na %d volta em minutos:\n",i); 
+scanf("%d",&tempo[i]); 
+if( tempo[i] > mt) { 
+mt=tempo[i]; 
+melhorvolta=i; 
+} 
+
+st=st+tempo[i]; 
+
+} 
+
+tmedio=st/10; 
+printf("\no melhor tempo foi %d.\n ",mt); 
+printf("\na volta q ocorreu o melhor tempo foi %d volta.\n ",melhorvolta); 
+printf("\ntempo medio das 10 voltas: %f\n ",tmedio);
+}

1d472e654d6dfa017597aad340be673e1960fbd6
Felipe Menegat <felipe_menegat@hotmail.com>
2017-09-14 23:14:44 -0300

    Update EXC7PVPG.c

--- EXC7PVPG.c
+++ EXC7PVPG.c
@@ -7,32 +7,24 @@ Aluno: Felipe Menegat
 #include <stdio.h>
 #include <stdlib.h>
 
+int main() {
+int vet[10],menor,maior=0;
 
-int main(int argc, char *argv[]) {
-
-int f[10];
-int d ;
-int maximo ;
-maximo = 0 ;
-int minimo ;
-	
-	for(d=0; d<10; d++){
-		printf("Digite o valor de d [%d]: ",d+1);
-		fflush(stdin);
-		scanf("%d",&f);
-		
-		if(f>maximo){
-		maximo=f;
-		}
-		
-		if(f<f(d-1)){
-		minimo=f;
-		}
-	}
-printf("Maximo e:%d\n",maximo);
-printf("Minimo e:%d\n",minimo);
-system("PAUSE");
-return 0;
+for (int i = 0; i < 10; ++i) {
+    printf("Digite o elemento %d\n",i+1);
+    scanf("%d",&vet[i]);
 }
 
+menor = vet[0];
+for (int i = 0; i < 10; ++i) {
+  if (vet[i]>maior) maior=vet[i];
+  if (vet[i]<menor) menor=vet[i];
+}
+
+printf("O menor numero=%d\nO maior numero=%d\n",menor,maior);
+
+
+system("pause");
+
+}
 
